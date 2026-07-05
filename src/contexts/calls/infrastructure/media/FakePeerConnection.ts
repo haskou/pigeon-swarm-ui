9cd14c672680a64b67ff2e9d75fd33a79248ec89
Haskō <102477627+haskou@users.noreply.github.com>
2026-07-05 23:17:11 +0200

    fix(calls): 🐛 Signal ringing call participants (#129)

--- src/contexts/calls/infrastructure/media/FakePeerConnection.ts
+++ src/contexts/calls/infrastructure/media/FakePeerConnection.ts
@@ -7,9 +7,21 @@ export type FakePeerConnection = Pick<
   | 'getSenders'
   | 'removeTrack'
 > & {
+  addIceCandidate: (
+    candidate?: RTCIceCandidate | RTCIceCandidateInit | null,
+  ) => Promise<void>;
   connectionState: RTCPeerConnectionState;
+  createAnswer: () => Promise<RTCSessionDescriptionInit>;
+  createOffer: () => Promise<RTCSessionDescriptionInit>;
   iceConnectionState: RTCIceConnectionState;
   localDescription: RTCSessionDescriptionInit | null;
+  remoteDescription: RTCSessionDescriptionInit | null;
   senders: RTCRtpSender[];
+  setLocalDescription: (
+    description?: RTCSessionDescriptionInit,
+  ) => Promise<void>;
+  setRemoteDescription: (
+    description: RTCSessionDescriptionInit,
+  ) => Promise<void>;
   signalingState: RTCSignalingState;
 };
